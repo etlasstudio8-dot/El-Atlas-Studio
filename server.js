198c08c686c620a41ee81b74ad322126a1d57b03
etlasstudio8-dot <etlasstudio8@gmail.com>
2026-07-28 20:03:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const allowedOrigins = [
   process.env.DASHBOARD_URL,
   // Firebase hosting
   'https://elatlas-studio.web.app',
-  'https://elatlas-studio.netlify.app',
   'https://elatlas-studio.firebaseapp.com',
   // Render (your admin HTML is served from here too)
   'https://el-atlas-studio.onrender.com',
