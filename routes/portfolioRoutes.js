cf95d4ffcdf3af7c6aad86df209f5a3975bb9649
etlasstudio8-dot <etlasstudio8@gmail.com>
2026-07-28 19:56:22 +0530

    Update portfolioRoutes.js

--- routes/portfolioRoutes.js
+++ routes/portfolioRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getAllPortfolio, getPortfolioById, createPortfolio, updatePortfolio, deletePortfolio } = require('../controllers/portfolioController');
+const { getAllPortfolio, getPortfolioById, createPortfolio, updatePortfolio, uploadProjectVideo, deletePortfolio } = require('../controllers/portfolioController');
 const { protect, checkPermission, adminOnly } = require('../middleware/auth');
+const { uploadSingleVideo, handleMulterError } = require('../middleware/upload');
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/', getAllPortfolio);
 router.get('/:id', getPortfolioById);
 router.post('/', protect, checkPermission('portfolio:edit'), upload.fields([{ name: 'mainImage', maxCount: 1 }, { name: 'images', maxCount: 10 }]), createPortfolio);
+router.post('/:id/video', protect, checkPermission('portfolio:edit'), uploadSingleVideo, handleMulterError, uploadProjectVideo);
 router.put('/:id', protect, checkPermission('portfolio:edit'), upload.fields([{ name: 'mainImage', maxCount: 1 }, { name: 'images', maxCount: 10 }]), updatePortfolio);
 router.delete('/:id', protect, adminOnly, deletePortfolio);
 
